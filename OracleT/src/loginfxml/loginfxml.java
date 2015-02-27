fcd27b0e1be9ca6d00c5609e4e353820b98c806e
Frode Naesje <fn@hin.no>
2015-02-27 12:51:35 +0100

    Kode funker etter Javascript innsetting

---
+++ OracleT/src/loginfxml/loginfxml.java
@@ -0,0 +1,23 @@
+package loginfxml;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class loginfxml extends Application {
+	@Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("loginfxml.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+	public static void main(String[] args) {
+        launch(args);
+    }
+}
